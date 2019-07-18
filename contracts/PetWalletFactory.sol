e22452a12008cb9406c965083305528d66d8a553
nguyen duy dien <nguyenduydien1311999@gmail.com>
2019-07-18 12:57:12 +0700

    add get all pet addresses of an owner function to factory

--- contracts/PetWalletFactory.sol
+++ contracts/PetWalletFactory.sol
@@ -6,9 +6,14 @@ contract PetWalletFactory {
   /*
    *  Events
    */
+
   event ContractInstantiation(uint petId, address petOwner, address instantiatin);
 
-  address[] public petAddresses;
+  /*
+   * Storage
+   */
+
+  mapping(address => address[]) public petAddresses;
 
   /// @dev Allows verified creation of a pet wallet.
   /// @return Returns wallet address.
@@ -17,9 +22,16 @@ contract PetWalletFactory {
     returns (address petAddress)
   {
     petAddress = address(new PetWallet(_ownerAddress, _petId));
-    petAddresses.push(petAddress);
+    petAddresses[_ownerAddress].push(petAddress);
     return petAddress;
 
     emit ContractInstantiation(_petId, _ownerAddress , petAddress);
+  }
+
+  /// @dev Allows to get all pet addresses of an user
+  /// @param _petOwner: an user address
+  /// @return Returns all pet addresses of an address.
+  function getAllPetAddressOf(address _petOwner) public view returns (address[] memory) {
+    return petAddresses[_petOwner];
   }
 }
