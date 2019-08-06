773735c7d09e455e8cd28a3f79f2be1187141d7d
Quang <ntquang22298@gmail.com>
2019-08-06 16:57:41 +0700

    feat: pet change size and change color

--- client/src/petData/Yeti/index.js
+++ client/src/petData/Yeti/index.js
@@ -1,7 +1,59 @@
 import WhiteYeti from './WhiteYeti';
 import PinkYeti from './PinkYeti';
 const Yeti = {
-  0: WhiteYeti,
-  1: PinkYeti
+  size: [
+    {
+      scale: 1,
+      milestone: 0
+    },
+    {
+      scale: 1.25,
+      milestone: 25
+    },
+    {
+      scale: 1.5,
+      milestone: 50
+    },
+    {
+      scale: 1.75,
+      milestone: 75
+    },
+    {
+      scale: 2,
+      milestone: 100
+    }
+  ],
+  progress: [
+    {
+      index: 0,
+      item: WhiteYeti,
+      milestone: 0,
+      src: ''
+    },
+    {
+      index: 1,
+      item: PinkYeti,
+      milestone: 25,
+      src: ''
+    },
+    {
+      index: 2,
+      item: PinkYeti,
+      milestone: 50,
+      src: ''
+    },
+    {
+      index: 3,
+      item: PinkYeti,
+      milestone: 75,
+      src: ''
+    },
+    {
+      index: 4,
+      item: PinkYeti,
+      milestone: 100,
+      src: ''
+    }
+  ]
 };
 export default Yeti;
